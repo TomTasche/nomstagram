8322dcf51b84fe4740b9324d1171ab1c4b33717a
Thomas Taschauer <tomtasche@gmail.com>
2014-09-05 21:39:34 +0200

    access deepbelief via JNA

--- vm/src/main/java/at/tomtasche/nomstagram/vm/MainApplication.java
+++ vm/src/main/java/at/tomtasche/nomstagram/vm/MainApplication.java
@@ -1,21 +1,29 @@
 package at.tomtasche.nomstagram.vm;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 public class MainApplication {
 
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+    public static void main(String[] args) throws Exception {
+        QueuedThreadPool threadPool = new QueuedThreadPool(100, 50);
+        Server server = new Server(threadPool);
 
-		ServletContextHandler context = new ServletContextHandler();
-		context.setContextPath("/");
-		context.addServlet(new ServletHolder(new TastyServlet()), "/*");
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
 
-		server.setHandler(context);
+        server.addConnector(connector);
 
-		server.start();
-		server.join();
-	}
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.addServlet(new ServletHolder(new TastyServlet()), "/*");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
 }
--- vm/src/main/java/at/tomtasche/nomstagram/vm/TastyServlet.java
+++ vm/src/main/java/at/tomtasche/nomstagram/vm/TastyServlet.java
@@ -1,11 +1,13 @@
 package at.tomtasche.nomstagram.vm;
 
-import java.io.BufferedReader;
+import com.jetpac.deepbelief.DeepBelief;
+import com.jetpac.deepbelief.DeepBelief.JPCNNLibrary;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.PointerByReference;
+
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.file.Files;
@@ -21,120 +23,104 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class TastyServlet extends HttpServlet {
 
-	private final Logger logger;
+    private static final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
 
-	private final File beliefDirectory;
-	private final File cacheDirectory;
+    private final Object beliefLock = new Object();
 
-	private File resultsFile;
+    private final Logger logger;
 
-	public TastyServlet() {
-		logger = Logger.getLogger("Beliefer");
+    private final File cacheDirectory;
 
-		beliefDirectory = new File("deepbelief");
-		cacheDirectory = new File("cache");
+    private Pointer networkHandle;
 
-		resultsFile = new File("results.txt");
-	}
+    public TastyServlet() {
+        logger = Logger.getLogger("Beliefer");
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// TODO:
-		// http://localhost:8080/?photoUrl=http%3A%2F%2Fwww.mcdonalds.ca%2Fcontent%2Fdam%2FCanada%2Fen%2FPromo%2Fblt%2Fimg%2Fburger.png
+        cacheDirectory = new File("cache");
+    }
 
-		String output = initializeBeliefer();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO:
+        // http://localhost:8080/?photoUrl=http%3A%2F%2Fwww.mcdonalds.ca%2Fcontent%2Fdam%2FCanada%2Fen%2FPromo%2Fblt%2Fimg%2Fburger.png
 
-		if (!cacheDirectory.exists()) {
-			cacheDirectory.mkdir();
-		}
+        if (!cacheDirectory.exists()) {
+            cacheDirectory.mkdir();
+        }
 
-		String photoUrl = req.getParameter("photoUrl");
-		photoUrl = URLDecoder.decode(photoUrl, "UTF-8");
+        String photoUrl = req.getParameter("photoUrl");
+        photoUrl = URLDecoder.decode(photoUrl, "UTF-8");
 
-		File tempFile = File.createTempFile("photo-", ".png", cacheDirectory);
+        File tempFile = File.createTempFile("photo-", ".png", cacheDirectory);
 
-		URL website = new URL(photoUrl);
-		logger.log(Level.SEVERE, website.toString());
-		Files.copy(website.openStream(), tempFile.toPath(),
-				StandardCopyOption.REPLACE_EXISTING);
+        URL website = new URL(photoUrl);
+        logger.log(Level.INFO, website.toString());
+        Files.copy(website.openStream(), tempFile.toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
 
-		// String output = executeCommand("./deepbelief burger.png",
-		// beliefDirectory);
+        if (networkHandle == null) {
+            logger.log(Level.INFO,
+                    "working directory " + new File("").getAbsolutePath());
 
-		output = executeCommand("./deepbelief " + tempFile.getAbsolutePath(),
-				beliefDirectory);
+            JPCNNLibrary beliefer = DeepBelief.JPCNNLibrary.INSTANCE;
+            networkHandle = beliefer.jpcnn_create_network(new File(
+                    "jetpac.ntwk").toString());
+        }
 
-		FileWriter fileWriter = new FileWriter(resultsFile);
-		fileWriter.append(System.getProperty("line.separator"));
-		fileWriter.append(output);
-		fileWriter.flush();
-		fileWriter.close();
+        Pointer imageHandle = JPCNNLibrary.INSTANCE
+                .jpcnn_create_image_buffer_from_file(tempFile.getAbsolutePath());
 
-		resp.setContentType("text/plain");
-		resp.getWriter().println(output);
-		resp.getWriter().flush();
+        PointerByReference predictionsValuesRef = new PointerByReference();
+        IntByReference predictionsLengthRef = new IntByReference();
+        PointerByReference predictionsNamesRef = new PointerByReference();
+        IntByReference predictionsNamesLengthRef = new IntByReference();
 
-		// TODO: delete files in future versions
-		// tempFile.delete();
-	}
+        long startTimestamp = System.currentTimeMillis();
 
-	private String initializeBeliefer() {
-		if (beliefDirectory.exists()) {
-			logger.log(Level.INFO, "beliefer already initialized");
+        synchronized (beliefLock) {
+            JPCNNLibrary.INSTANCE.jpcnn_classify_image(networkHandle,
+                    imageHandle, 0, 0, predictionsValuesRef,
+                    predictionsLengthRef, predictionsNamesRef,
+                    predictionsNamesLengthRef);
+        }
 
-			return "";
-		}
+        long stopTimestamp = System.currentTimeMillis();
+        long duration = stopTimestamp - startTimestamp;
 
-		logger.log(Level.INFO, "beliefer not yet initialized");
+        logger.log(Level.INFO, "jpcnn_classify_image() took " + duration
+                + " ms");
 
-		beliefDirectory.mkdir();
+        JPCNNLibrary.INSTANCE.jpcnn_destroy_image_buffer(imageHandle);
 
-		String output = executeCommand(
-				"wget -nv -O belief.zip http://ge.tt/api/1/files/9H5Ygvq1/0/blob?download",
-				beliefDirectory);
+        Pointer predictionsValuesPointer = predictionsValuesRef.getValue();
+        int predictionsLength = predictionsLengthRef.getValue();
+        Pointer predictionsNamesPointer = predictionsNamesRef.getValue();
 
-		logger.log(Level.INFO, "beliefer downloaded");
+        float[] predictionsValues = predictionsValuesPointer.getFloatArray(0,
+                predictionsLength);
+        Pointer[] predictionsNames = predictionsNamesPointer.getPointerArray(0);
 
-		output = executeCommand("unzip belief.zip", beliefDirectory);
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < predictionsLength; i += 1) {
+            float predictionValue = predictionsValues[i];
 
-		logger.log(Level.INFO, "beliefer already unzipped");
+            if (predictionValue > 0.1) {
+                String name = predictionsNames[i].getString(0);
+                builder.append(String.format("%s = %f", name, predictionValue));
+                builder.append(LINE_SEPARATOR);
+            }
+        }
 
-		// output = executeCommand("./deepbelief", beliefDirectory);
+        // TODO: destroy network on server shutdown
+        // JPCNNLibrary.INSTANCE.jpcnn_destroy_network(networkHandle);
 
-		return output;
-	}
+        resp.setContentType("text/plain");
+        resp.getWriter().println(builder.toString());
+        resp.getWriter().flush();
 
-	private String executeCommand(String command, File directory) {
-		StringBuffer output = new StringBuffer();
-
-		try {
-			Process process = Runtime.getRuntime().exec(command, null,
-					directory);
-			process.waitFor();
-
-			output.append(streamToString(process.getInputStream()));
-			output.append(streamToString(process.getErrorStream()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return output.toString();
-	}
-
-	private String streamToString(InputStream stream) throws IOException {
-		StringBuilder builder = new StringBuilder();
-
-		BufferedReader reader = new BufferedReader(
-				new InputStreamReader(stream));
-
-		String line = "";
-		while ((line = reader.readLine()) != null) {
-			builder.append(line + "\n");
-
-			logger.log(Level.FINE, line);
-		}
-
-		return builder.toString();
-	}
+        // TODO: delete files in future versions
+        // tempFile.delete();
+    }
 }
